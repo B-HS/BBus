566bad2eae639105d906913dd08b55c886e1338b
Hyunseok Byun <gumyoincirno@gmail.com>
2023-02-15 20:33:07 +0900

    Set .env for keyfile

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,17 @@
 module.exports = {
-  plugins: ['react-native-reanimated/plugin'],
-  presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+        ["react-native-reanimated/plugin"],
+        [
+            "module:react-native-dotenv",
+            {
+                envName: "APP_ENV",
+                moduleName: "@env",
+                path: "key.env",
+                safe: false,
+                allowUndefined: false,
+                verbose: false,
+            },
+        ],
+    ],
+    presets: ["module:metro-react-native-babel-preset"],
 };
